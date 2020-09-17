e1c4828d1a3af60dd9764cda5997c1ab8f2f695f
enriqueMontano <montanoenrique@outlook.com>
2020-09-17 22:00:52 +0200

    Create user list component layout

--- src/components/templates/UserList/UserList.component.jsx
+++ src/components/templates/UserList/UserList.component.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { UserListWrapper } from './UserList.styled';
+import { Card } from '../../organisms';
 
 export const UserList = ({ error, pending, users }) => {
   return (
-    <UserListWrapper>{console.log(error, pending, users)}</UserListWrapper>
+    <UserListWrapper>
+      {console.log(error, pending, users)}
+      {users.items && users.items.map((user) => <Card key={user.id} />)}
+    </UserListWrapper>
   );
 };
--- src/components/templates/UserList/UserList.styled.js
+++ src/components/templates/UserList/UserList.styled.js
@@ -1,3 +1,11 @@
 import styled from 'styled-components';
 
-export const UserListWrapper = styled.div``;
+export const UserListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 40% 40%;
+  justify-content: center;
+  justify-items: center;
+  column-gap: 10px;
+  row-gap: 40px;
+  margin: 5% auto;
+`;
