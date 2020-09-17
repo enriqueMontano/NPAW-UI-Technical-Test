f6b07d8680645ad68b484deab515d8e286620d84
enriqueMontano <montanoenrique@outlook.com>
2020-09-17 22:01:20 +0200

    Add styles to Card component

--- src/components/organisms/Card/Card.styled.js
+++ src/components/organisms/Card/Card.styled.js
@@ -1,3 +1,12 @@
 import styled from 'styled-components';
 
-export const CardWrapper = styled.div``;
+export const CardWrapper = styled.div`
+  width: 400px;
+  height: 200px;
+
+  background-color: ${({ theme }) => theme.primary.gray};
+  box-shadow: 0px 0px 45px rgba(10, 16, 75, 0.1);
+  border-radius: 2px;
+  padding: 10px;
+  position: relative;
+`;
