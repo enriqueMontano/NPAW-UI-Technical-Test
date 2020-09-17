49141aaf13ff6d538ea3f67f446f5113b19368a2
enriqueMontano <montanoenrique@outlook.com>
2020-09-17 12:25:21 +0200

    Refactor user-fetching logic for more reusability

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchUsers } from './redux/action';
 import { Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import { UserList, UserDetails } from './components/templates';
 import './App.css';
+import { Navbar } from './components/organisms';
 
 function App() {
+  const [input, setInput] = useState('enrique');
+
+  const users = useSelector((state) => state.users);
+  const error = useSelector((state) => state.error);
+  const pending = useSelector((state) => state.pending);
+
+  const dispatch = useDispatch();
+
+  const handleInput = (value) => setInput(value);
+
+  useEffect(() => {
+    dispatch(fetchUsers(input));
+  }, [dispatch, input]);
+
   return (
     <ThemeProvider theme={theme}>
       <div className='App'>
+        <Navbar title='Explorer' action={handleInput} />
         <Switch>
-          <Route exact path='/' component={UserList} />
+          <Route
+            exact
+            path='/'
+            component={() => (
+              <UserList error={error} pending={pending} users={users} />
+            )}
+          />
           <Route exact path='#' component={UserDetails} />
         </Switch>
       </div>
--- src/components/templates/UserList/UserList.component.jsx
+++ src/components/templates/UserList/UserList.component.jsx
@@ -1,28 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchUsers } from '../../../redux/action';
+import React from 'react';
 import { UserListWrapper } from './UserList.styled';
-import { Navbar } from '../../organisms';
 
-export const UserList = () => {
-  const [input, setInput] = useState('enrique');
-
-  const users = useSelector((state) => state.users);
-  const error = useSelector((state) => state.error);
-  const pending = useSelector((state) => state.pending);
-
-  const dispatch = useDispatch();
-
-  const handleInput = (value) => setInput(value);
-
-  useEffect(() => {
-    dispatch(fetchUsers(input));
-  }, [dispatch, input]);
-
+export const UserList = ({ error, pending, users }) => {
   return (
-    <UserListWrapper>
-      {console.log(error, pending, users, input)}
-      <Navbar title='Explorer' action={handleInput} />
-    </UserListWrapper>
+    <UserListWrapper>{console.log(error, pending, users)}</UserListWrapper>
   );
 };
