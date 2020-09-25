26ea61d51b450da1d1df51b5883b328b416c6466
enriqueMontano <montanoenrique@outlook.com>
2020-09-26 00:01:40 +0200

    Move bar chart to user list component

--- src/components/templates/UserList/UserList.component.jsx
+++ src/components/templates/UserList/UserList.component.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { UserListWrapper } from './UserList.styled';
+import { BarChart } from '../../atoms';
 import { Card } from '../../organisms';
 
-export const UserList = ({ data, action }) => {
+export const UserList = ({ data, action, chartData }) => {
   return (
     <UserListWrapper>
-      {console.log(data)}
-      {data &&
-        data.items.map((user) => (
-          <Card key={user.id} username={user.login} action={action} />
-        ))}
+      <BarChart indexBy='login' keys={['followers']} data={chartData} />
+      <div className='card-container'>
+        {data &&
+          data.items.map((user) => (
+            <Card
+              key={user.id}
+              username={user.login}
+              action={action}
+              followers={user.followers_url}
+            />
+          ))}
+      </div>
     </UserListWrapper>
   );
 };
--- src/components/templates/UserList/UserList.styled.js
+++ src/components/templates/UserList/UserList.styled.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
 export const UserListWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 40% 40%;
-  justify-content: center;
-  justify-items: center;
-  column-gap: 10px;
-  row-gap: 40px;
-  margin: 5% auto;
+  .card-container {
+    display: grid;
+    grid-template-columns: 40% 40%;
+    justify-content: center;
+    justify-items: center;
+    column-gap: 10px;
+    row-gap: 40px;
+    margin: 5% auto;
+  }
 `;
